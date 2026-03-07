fd27211504928c0a45acd22aeb02c483fdda89fe
Kanika <065030@fsm.ac.in>
2026-03-07 21:49:27 +0530

    commit

--- utils/prediction.py
+++ utils/prediction.py
@@ -1,36 +1,42 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import tensorflow as tf
-tf.get_logger().setLevel('ERROR')
 import joblib
 import streamlit as st
+import numpy as np
+try:
+    import tflite_runtime.interpreter as tflite
+except ImportError:
+    import tensorflow.lite as tflite
 
 @st.cache_resource
 def load_model_and_encoder():
-    """
-    Load the saved model and label encoder from the model directory.
-    Uses @st.cache_resource to load them only once.
-    """
-    model_path = "model/grocery_model.h5"
+    model_path = "model/grocery_model.tflite"
     encoder_path = "model/label_encoder.pkl"
-    
+
     if not os.path.exists(model_path) or not os.path.exists(encoder_path):
-        # We handle the case where the user has not uploaded the model yet
-        # Returning None to let the UI display an error
         return None, None
-    
-    model = tf.keras.models.load_model(model_path)
+
+    interpreter = tflite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
     label_encoder = joblib.load(encoder_path)
-    return model, label_encoder
+
+    return (interpreter, input_details, output_details), label_encoder
+
 
 def get_predictions(model, label_encoder, preprocessed_img):
-    """
-    Perform CNN inference and return top-5 categories.
-    """
-    preds = model.predict(preprocessed_img)[0]
+    interpreter, input_details, output_details = model
+
+    interpreter.set_tensor(input_details[0]['index'], preprocessed_img.astype('float32'))
+    interpreter.invoke()
+
+    preds = interpreter.get_tensor(output_details[0]['index'])[0]
+
     top_5_indices = preds.argsort()[-5:][::-1]
-    
+
     top_5_categories = [label_encoder.inverse_transform([i])[0] for i in top_5_indices]
     top_5_probabilities = [float(preds[i]) for i in top_5_indices]
-    
+
     return list(zip(top_5_categories, top_5_probabilities))
